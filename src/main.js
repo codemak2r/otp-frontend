70c28b4b1cb9134bd4a4ed82c2f85e27a6c96365
jackson <a403481704@gmail.com>
2021-05-27 18:13:05 +0800

    add elementUI

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入 elementUI 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 如果想要中文版 element-ui，按如下方式声明
+Vue.use(ElementUI)
+
+
 Vue.config.productionTip = false
 
 new Vue({
